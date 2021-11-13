d485aa42c3b8772d0898276863b514ad1b390563
Arthurdw <mail@arthurdw.com>
2021-11-13 16:21:25 +0100

    :sparkles: Base gateway type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 export type MessageTypes = "text" | "document" | "image";
 
-export interface Message {
-    content: string;
-    type: MessageTypes;
-    timestamp: number;
+export enum OpCode {
+  hello = 0 << 1,
+}
+
+export interface GatewayMessage {
+  op: OpCode;
+}
+
+export interface Message extends GatewayMessage {
+  content: string;
+  type: MessageTypes;
+  timestamp: number;
 }
