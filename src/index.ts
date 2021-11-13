c57f81bfc2729d645755ca0bb57cb26cbe54dcb1
Arthurdw <mail@arthurdw.com>
2021-11-13 17:20:02 +0100

    :sparkles: Opcodes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 export type MessageTypes = "text" | "document" | "image";
 
 export enum OpCode {
-  hello = 0 << 1,
+  hello = 0,
+  auth_start = 1,
+  auth_reply = 2
 }
 
 export interface GatewayMessage {
+    /** The gateway opcode. */
   op: OpCode;
+}
+
+export interface AuthStartMessage extends GatewayMessage {
+    /** Public client key. */
+    pub: string;
+}
+
+export interface AuthReplyMessage extends GatewayMessage {
+    /** The encrypted AES key. */
+    key: string;
 }
 
 export interface Message extends GatewayMessage {
